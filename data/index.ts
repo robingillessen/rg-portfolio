ed551042fae9aa7a785c35e3f866565bc1f3bbee
Robin <robinlaurentius@gmail.com>
2024-05-27 09:36:30 +0200

    chore: Update Hero component to include min-height for better layout consistency

--- data/index.ts
+++ data/index.ts
@@ -49,8 +49,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "An e-commerce project for a client in the Netherlands.",
+    description: "Currently working on",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
